6c12d935253d176f8234d224e45f9118ec88bf00
harshparmar <parmarharsh4296@gmail.com>
2022-04-12 20:53:26 -0400

    Revert "a8 MONGO sp22"
    This reverts commit 81a4bcfd9547995238a0e133132c63c2828e6385.

--- controller/tuits-controller.js
+++ controller/tuits-controller.js
@@ -1,27 +1,42 @@
-import * as tuitsDao from "./tuits-dao.js";
+import posts from "./tuits.js";
+let tuits = posts;
 
-const createTuit = async (req, res) => {
-    const newTuit = req.body;
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(insertedTuit);
+const createTuit = (req, res) => {
+    console.log("Create called");
+    console.log(req)
+    const newTuit = {};
+    const profile = req.body.profile;
+    newTuit.postedBy = {
+        "username": profile.firstName + ' ' + profile.lastName
+    };
+    newTuit.handle = profile.handle;
+    newTuit.time = 'now';
+    newTuit['logo-image'] = profile.profilePicture;
+    newTuit['avatar-image'] = profile.profilePicture;
+    newTuit.tuit = req.body.data.tuit;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.stats = {
+        "comments": 0,
+        "retuits": 0,
+        "likes": 0,
+        "dislikes": 0
+    };
+    tuits.unshift(newTuit);
+    res.json(newTuit);
 }
 
-const findAllTuits = async (req, res) => {
-    const tuits = await tuitsDao.findAllTuits();
-    res.json(tuits);
-}
-
-const updateTuit = async (req, res) => {
+const findAllTuits = (req, res) => res.json(tuits);
+const updateTuit = (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
-    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
-    res.sendStatus(status);
+    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+    res.sendStatus(200);
 }
 
-const deleteTuit = async (req, res) => {
+const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-    res.sendStatus(status);
+    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
 }
 
 
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ import helloController from "./controller/hello-controller.js";
 import userController from "./controller/user-controller.js";
 import tuitController from "./controller/tuits-controller.js";
 import cors from 'cors';
-import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev';
-mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -15,4 +12,4 @@ helloController(app);
 userController(app);
 tuitController(app);
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT ||4000);
